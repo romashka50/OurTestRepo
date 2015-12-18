e7a1c35d6b2af5174c73b5a732acd06086af6853
romashka50 <roma.buchuk@gmail.com>
2015-12-18 15:06:09 +0200

    Backbone first commit

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+require.config({
+    paths: {
+        jQuery       : './libs/jquery/dist/jquery',
+        Underscore   : '/js/libs/underscore/underscore',
+        Backbone     : './libs/backbone/backbone'
+    },
+    shim : {
+        'Backbone'  : ['Underscore', 'jQuery'],
+        'app'       : ['Backbone']
+    }
+});
+
+require(['app/app'], function(app){
+    app.initialize();
+});
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var consolidate = require('consolidate');
+
+app.use(express.static(__dirname + '/public'));
+
+app.engine('html', consolidate.ejs);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res, next){
+    res.render('index.html', {title: 'Hello World Rendered'});
+});
+
+app.listen(3030, function(){
+    console.log('Server start listening on port', 3030);
+});
