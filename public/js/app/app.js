4bfeb263e61f5fc0af22ba742da3565f30cb2675
romashka50 <roma.buchuk@gmail.com>
2015-12-21 13:18:11 +0200

    without listenTo

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,31 +1,26 @@
-define(['/js/models/song.js', '/js/collections/songs.js'], function (SongModel, SongCollection) {
+define(['/js/models/song.js', '/js/collections/songs.js'], function (SongModel, SongCollection, View) {
     function initialize() {
         var song = new SongModel({
             id: 1,
             title: 'Marry Christmas'
         });
 
-        var o = {
-            save: function(data){
-                $('#title').text(data.title);
-            }
-        };
-        _.extend(o, Backbone.Events);
-
-        o.listenTo(song, 'myCustomEvent', function(){
-            this.save(song.changed);
-        });
-
         song.save(null, {
             wait: true,
             success: function(model, xhr){
                 song.set('title', 'another one');
-                song.trigger('myCustomEvent', {tile: 'sdgfjsdgjhsdgf'});
+                song.trigger('myCustomEvent');
             },
             error: function(){
 
             }
         });
+
+        function doSomethingWhenEvent(){ //ToDo read on http://backbonejs.org/#Events
+            alert('We listening ' + this.get('title')); // this.get('title') === this.attributes.title || this.toJSON().title
+        };
+
+        song.on('myCustomEvent', doSomethingWhenEvent, song); // toDo read default Model trigered events {change, change:attr1,}
     }
 
     return {
